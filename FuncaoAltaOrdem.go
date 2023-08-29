19f3355689cd1737f8aadb2fdb7e171c01153b09
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-08-28 23:30:12 -0300

    Implementada a segunda parte de função de alta ordem

--- FuncaoAltaOrdem.go
+++ FuncaoAltaOrdem.go
@@ -5,6 +5,12 @@ import (
 	"math"
 )
 
+//Função como argumento
+func CalculaVolumeEsfera(vol func(radius float64) float64) {
+	fmt.Println("Volume da esfera é:", vol(3))
+}
+
+
 //Função retornando uma função
 func CalcularVolumeEsfera() func(radius float64) float64 {
 	resultado := func(radius float64) float64 {
@@ -16,6 +22,13 @@ func CalcularVolumeEsfera() func(radius float64) float64 {
 }
 
 func main() {
-	sVol := CalcularVolumeEsfera()
-	fmt.Println("Volume da esfera é:", sVol(5))
+	volumeEsfera := func(radius float64) float64 {
+		resultado := 4 /3 * math.Pi * radius * radius * radius
+		return resultado
+	}
+
+	CalculaVolumeEsfera(volumeEsfera)
+
+	//sVol := CalcularVolumeEsfera()
+	//fmt.Println("Volume da esfera é:", sVol(5))
 }
